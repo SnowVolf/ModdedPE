0b4113dcee57b5ba3f38220abd6e3c60457f3e53
TimScriptov <timscriptov@mail.ru>
2019-07-11 23:28:35 +0500

    Fixes x86.hpp substrate

--- app/src/main/jni/substrate/Android.mk
+++ app/src/main/jni/substrate/Android.mk
@@ -1,8 +1,7 @@
 LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
+include $(CLEAR_VARS)  
 LOCAL_LDLIBS := -llog
 LOCAL_MODULE    := substrate
-LOCAL_SRC_FILES := hde64.c Debug.cpp Hooker.cpp PosixMemory.cpp
-include $(BUILD_SHARED_LIBRARY)
+LOCAL_SRC_FILES := hde64.c Hooker.cpp Debug.cpp PosixMemory.cpp
+
+include $(BUILD_SHARED_LIBRARY)  
--- app/src/main/jni/substrate/Application.mk
+++ app/src/main/jni/substrate/Application.mk
@@ -2,5 +2,5 @@
 APP_STL := gnustl_static
 APP_ABI := armeabi armeabi-v7a arm64-v8a x86 x86_64 mips mips64
 APP_PLATFORM := android-16
-APP_CFLAGS := -O2
+APP_CFLAGS := -O2 -Wno-logical-op-parentheses
 APP_STL := system
